b52b702167f12a541856716288970a7273e07219
Александра <aleksandrasilina@gmail.com>
2024-10-01 15:14:27 +0300

    add habits/ urls.py

---
+++ habits/urls.py
@@ -0,0 +1,12 @@
+from rest_framework.routers import SimpleRouter
+
+from habits.apps import HabitsConfig
+from habits.views import HabitViewSet
+
+app_name = HabitsConfig.name
+
+router = SimpleRouter()
+router.register("", HabitViewSet)
+
+urlpatterns = []
+urlpatterns += router.urls
