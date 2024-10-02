2a49cd8e7c1642025045aba745cba9f0db2f7670
Александра <aleksandrasilina@gmail.com>
2024-10-02 11:41:15 +0300

    change habits/ urls.py add PublicHabitListAPIView, PublicHabitRetrieveAPIView

--- habits/urls.py
+++ habits/urls.py
@@ -1,12 +1,16 @@
+from django.urls import path
 from rest_framework.routers import SimpleRouter
 
 from habits.apps import HabitsConfig
-from habits.views import HabitViewSet
+from habits.views import HabitViewSet, PublicHabitListAPIView, PublicHabitRetrieveAPIView
 
 app_name = HabitsConfig.name
 
 router = SimpleRouter()
 router.register("", HabitViewSet)
 
-urlpatterns = []
+urlpatterns = [
+    path("public/", PublicHabitListAPIView.as_view(), name="public-list"),
+    path("public/<int:pk>/", PublicHabitRetrieveAPIView.as_view(), name="public-retrieve"),
+]
 urlpatterns += router.urls
