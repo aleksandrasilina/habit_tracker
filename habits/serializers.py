3f677837460308d12bc2d1ac08e652598aaed856
Александра <aleksandrasilina@gmail.com>
2024-10-03 14:49:05 +0300

    change habits/ serializers.py

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 
 from habits.models import Habit
-from habits.validators import RelatedHabitOrRewardValidator, validate_duration, RelatedHabitValidator, \
-    EnjoyableHabitValidator, PeriodicityValidator
+from habits.validators import (EnjoyableHabitValidator, PeriodicityValidator,
+                               RelatedHabitOrRewardValidator,
+                               RelatedHabitValidator, validate_duration)
 
 
 class HabitSerializer(serializers.ModelSerializer):
@@ -14,6 +15,10 @@ class HabitSerializer(serializers.ModelSerializer):
         validators = [
             RelatedHabitOrRewardValidator(field_1="related_habit", field_2="reward"),
             RelatedHabitValidator(field="related_habit"),
-            EnjoyableHabitValidator(is_enjoyable="is_enjoyable", related_habit="related_habit", reward="reward"),
+            EnjoyableHabitValidator(
+                is_enjoyable="is_enjoyable",
+                related_habit="related_habit",
+                reward="reward",
+            ),
             PeriodicityValidator(field="periodicity"),
         ]
