2f3ec3e0c3ac4d24a766c89910b13a6eb4630aa9
Александра <aleksandrasilina@gmail.com>
2024-10-03 15:45:55 +0300

    change habits/views.py add docstrings and method_decorators

--- habits/views.py
+++ habits/views.py
@@ -10,13 +10,13 @@ from habits.serializers import HabitSerializer
 from users.permissions import IsCreator
 
 
-@method_decorator(name='update', decorator=swagger_auto_schema(operation_description="Изменение привычки"))
-@method_decorator(name='destroy', decorator=swagger_auto_schema(operation_description="Удаление привычки"))
-@method_decorator(name='create', decorator=swagger_auto_schema(operation_description="Создание привычки"))
-@method_decorator(name='retrieve', decorator=swagger_auto_schema(operation_description="Информация о привычке"))
-@method_decorator(name='list', decorator=swagger_auto_schema(operation_description="Список привычек"))
+@method_decorator(name='update', decorator=swagger_auto_schema(operation_description="Изменение привычки."))
+@method_decorator(name='destroy', decorator=swagger_auto_schema(operation_description="Удаление привычки."))
+@method_decorator(name='create', decorator=swagger_auto_schema(operation_description="Создание привычки."))
+@method_decorator(name='retrieve', decorator=swagger_auto_schema(operation_description="Информация о привычке."))
+@method_decorator(name='list', decorator=swagger_auto_schema(operation_description="Список привычек."))
 class HabitViewSet(ModelViewSet):
-    """Вьюсет для привычек"""
+    """Вьюсет для привычек."""
 
     queryset = Habit.objects.all()
     serializer_class = HabitSerializer
@@ -44,20 +44,20 @@ class HabitViewSet(ModelViewSet):
         return super().get_permissions()
 
     def partial_update(self, request, *args, **kwargs):
-        """Частичное изменение привычки"""
+        """Частичное изменение привычки."""
 
         return super().partial_update(request, *args, **kwargs)
 
 
 class PublicHabitListAPIView(ListAPIView):
-    """Список публичных привычек"""
+    """Список публичных привычек."""
 
     queryset = Habit.objects.filter(is_public=True)
     serializer_class = HabitSerializer
 
 
 class PublicHabitRetrieveAPIView(RetrieveAPIView):
-    """Информация о публичной привычке"""
+    """Информация о публичной привычке."""
 
     queryset = Habit.objects.filter(is_public=True)
     serializer_class = HabitSerializer
