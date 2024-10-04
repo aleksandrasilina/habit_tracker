43662cb5f4558548075387a58737dc7003793fed
Александра <aleksandrasilina@gmail.com>
2024-10-04 16:15:43 +0300

    change users/ tests.py

--- users/tests.py
+++ users/tests.py
@@ -2,15 +2,14 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from habits.models import Habit
 from users.models import User
 
 
 class UserTestCase(APITestCase):
-    """Класс для тестирования пользователя"""
+    """Класс для тестирования пользователя."""
 
     def test_user_create(self):
-        """Тестирование создания пользователя"""
+        """Тестирование создания пользователя."""
 
         url = reverse("users:register")
         data = {"email": "user_1@email.com", "password": 123456}
